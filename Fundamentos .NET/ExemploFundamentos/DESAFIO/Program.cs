0f799f0d380f9bf7aef888fef9b1e1aaa07c8d88
Full Stack Developer <69222299+pedro-ddias@users.noreply.github.com>
2023-08-09 16:17:28 -0300

    Fundamentos - completo

---
+++ Fundamentos .NET/ExemploFundamentos/DESAFIO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Seja bem vindo ao Sistema de Estacionamento");
+        Console.WriteLine("Digite o valor inicial");
+        string valorInicial = Console.ReadLine();
+
+        Console.WriteLine("Digite sua opção");
+        Console.WriteLine("1 - Cadastrar Veículo\n2 - Remover Veículo");
+    }
+}
+
+
---
+++ Fundamentos .NET/ExemploFundamentos/ExemploFundamentos.Common/Models/Pessoa.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Common.Models
+{
+    /// <summary>
+    /// Representa uma pessoa física
+    /// </summary>
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+
+        /// <summary>
+        /// Apresentação da pessoa, falando seu nome e idade
+        /// </summary>
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+        }
+    }
+}
